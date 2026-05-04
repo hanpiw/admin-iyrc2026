675676bfcefb00c33b1ed47046cb37ddc9dfc454
hanpiw <alhafidz.almagribi409@gmail.com>
2026-05-04 10:06:41 +0700

    feat: update level

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -23,41 +23,41 @@ export const LOMBA_HIERARCHY: Record<string, CategoryHierarchy> = {
   },
   "2 On 2 Soccer": {
     name: "2 On 2 Soccer",
-    levels: ["Junior A", "Junior B", "Junior C", "Senior", "Open"]
+    levels: ["Open"]
   },
   "Coding Mission": {
     name: "Coding Mission",
     subCategories: [
-      { name: "MRT", levels: ["Junior B", "Junior C", "Senior"] },
-      { name: "RACERO", levels: ["Junior B", "Junior C", "Senior"] }
+      { name: "MRT", levels: ["Junior BC", "Senior"] },
+      { name: "RACERO", levels: ["Junior BC", "Senior"] }
     ]
   },
   "AI Animation": {
     name: "AI Animation",
-    levels: ["Junior B", "Junior C", "Senior"]
+    levels: ["Junior BC", "Senior"]
   },
   "Creative": {
     name: "Creative",
     subCategories: [
-      { name: "MRT", levels: ["Junior A", "Junior B", "Junior C", "Senior"] },
-      { name: "Open", levels: ["Junior A", "Junior B", "Junior C", "Senior", "Open"] },
-      { name: "Online", levels: ["Junior A", "Junior B", "Junior C", "Senior", "Open"] }
+      { name: "MRT", levels: ["Junior ABC", "Senior"] },
+      { name: "Open", levels: ["Open"] },
+      { name: "Online", levels: ["Open"] }
     ]
   },
   "Game Maker Kit": {
     name: "Game Maker Kit",
-    levels: ["Junior A", "Junior B", "Junior C", "Senior", "Open"]
+    levels: ["Open"]
   },
   "Drone Soccer": {
     name: "Drone Soccer",
-    levels: ["Junior A", "Junior B", "Junior C", "Senior", "Open"]
+    levels: ["Open"]
   },
   "Item Recycle": {
     name: "Item Recycle",
-    levels: ["Junior B", "Junior C", "Senior"]
+    levels: ["Junior BC", "Senior"]
   },
   "Robot Teather": {
     name: "Robot Teather",
-    levels: ["Junior A", "Junior B", "Junior C", "Senior"]
+    levels: ["Junior ABC", "Senior"]
   }
 }
